64a9e5d160f3ad542c04eeb3ed828d771c435794
Nick <nick@nickkelleystuff.com>
2020-06-01 11:41:21 -0700

    fix meta

--- ember-subroute/ember-cli-build.js
+++ ember-subroute/ember-cli-build.js
@@ -8,6 +8,7 @@ module.exports = function(defaults) {
     fingerprint: {
       enabled: false,
     },
+    storeConfigInMeta: false,
   });
 
   // Use `app.import` to add additional libraries to the generated
